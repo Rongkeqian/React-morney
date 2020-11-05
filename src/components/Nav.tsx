440b96e600537b8b10f6b377250088e6017709c0
RongKeQian <qianer_lover@126.com>
2020-11-05 20:25:44 +0800

    添加icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,28 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/statistics.svg')
+require("icons/tags.svg")
+
+
 
 const NavWrapper = styled.div`
 box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul {
       display: flex;
-      
     > li{
-        flex-grow: 1;
-        padding:16px;
-        height: 56px;
+        width:33.3333%;
+        padding:4px;
+        text-align: center;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
+        .icon{
+        width:24px;
+        height: 24px;
+        }
     }
   }
 `
@@ -22,12 +31,21 @@ const Nav=()=>{
     <NavWrapper>
       <ul>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#tags'/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#money'/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#statistics'/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
