d4f3732b88aa8f3d30427a1748604c94cfa7e56f
RongKeQian <qianer_lover@126.com>
2020-11-05 09:29:00 +0800

    配置路径

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
 
-function App() {
+ function App() {
   return (
-    <div>
-        hello
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from ='/' to='/money' />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+function Statistics() {
+  return <h2>统计页面</h2>;
+}
+
+function Tags() {
+  return <h2>标签页面</h2>;
+}
+
+function Money() {
+  return <h2>记账页面</h2>;
+}
+function NoMatch() {
+  return (
+    <div>
+      页面不存在，地址错误
+    </div>
+  );
+}
 export default App;
