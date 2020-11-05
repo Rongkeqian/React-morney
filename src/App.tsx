eaa1b4469b78b236875761c67fd5f5bc24cc420e
RongKeQian <qianer_lover@126.com>
2020-11-05 21:21:14 +0800

    修改标签

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import {HashRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav'
-
-const Wrapper = styled.div`
-border:1px solid red;
-height: 100vh;
-display: flex;
-flex-direction: column;
-
-`;
-const Main = styled.div`
-flex:1;
-overflow: auto;
-`;
+import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
           <Switch>
             <Route path="/tags">
               <Tags/>
@@ -35,23 +20,28 @@ function App() {
               <NoMatch/>
             </Route>
           </Switch>
-        </Main>
-        <Nav />
-      </Wrapper>
     </Router>
   );
 }
 
 function Statistics() {
-  return <h2>统计页面</h2>;
+  return(
+    <Layout>
+      <h2>统计</h2>
+    </Layout>
+  );
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return(
+ <Layout><h2>标签页面</h2></Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return(
+  <Layout><h2>记账</h2></Layout>
+  );
 }
 
 function NoMatch() {
