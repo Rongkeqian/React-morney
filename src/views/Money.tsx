b57c03fa77c97c77b4612059c29fae02490d4dc5
RongKeQian <qianer_lover@126.com>
2020-11-06 19:19:28 +0800

    记账页面ui完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagSections = styled.section`
+  flex-grow:1;
   background:#FFF;
   padding:0 16px;
+  display:flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   >ol{
   margin: 0 -12px;
     >li{
@@ -19,22 +24,97 @@ const TagSections = styled.section`
   }
   >button{
     border:none;
+    border-bottom:1px solid #333;
     font-family: inherit;
     padding:2px 4px;
-    border-bottom:1px solid #333;
     color:#666;
     margin-top:16px;
   }
 `
-const NotesSection =styled.section``
+const NotesSection =styled.section`
+background: #f5f5f5;
+padding:0 16px;
+font-size: 14px;
+> label{
+display: flex;
+align-items: center;
+  >span{
+    margin-right: 16px;
+    white-space: nowrap;
+  }
+  > input{
+    display: block;
+    flex:1;
+    height: 72px;
+    border:none;
+    background:none
+  }
+}
+`
 
-const CategorySection = styled.section``
+const CategorySection = styled.section`
+>ul{
+display: flex;
+background:#c4c4c4;
+font-size: 24px;
+  >li{
+    width: 50%;
+    text-align:center;
+    padding:18px 0;
+    position: relative;
+    &.selected:after{
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 4px;
+      width: 100%;
+      background: #333;
+    }
+  }
+}
+`
 
-const NumberPadSection = styled.section``
-
+const NumberPadSection = styled.section`
+display:flex;
+flex-direction: column;
+> .output{
+  background: #fff;
+  font-size:36px;
+  line-height: 72px;
+  text-align:right;
+  padding:0 16px;
+  box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+              inset 0 5px 5px -5px rgba(0,0,0,0.25);
+}
+> .pad{
+  float: left;
+   >button{
+      width:25%;
+      height: 64px;
+      font-size: 18px;
+      border:none;
+      border-bottom:1px solid #f2f2f2;
+      line-height: 1.2;
+      border-bottom:1px solid #d9d9d9;
+      border-right: 1px solid #d9d9d9;
+      &.ok{
+            height: 128px;
+            float: right;
+      }
+      &.zero{
+      width: 50%;
+      }
+   }
+}
+`
+const MyLayout =styled(Layout)`
+display: flex;
+flex-direction: column;
+`
 function Money() {
   return(
-    <Layout>
+    <MyLayout className = 'xxx'>
        <TagSections>
           <ol>
             <li>衣</li>
@@ -47,18 +127,18 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type='text'/>
+          <input type='text' placeholder='在这里添加备注'/>
         </label>
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className='selected'>支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -70,12 +150,12 @@ function Money() {
           <button>7</button>
           <button>8</button>
           <button>9</button>
-         <button>OK</button>
-          <button>0</button>
+         <button className='ok'>OK</button>
+          <button className='zero'>0</button>
           <button>.</button>
         </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   );
 }
 export default Money
