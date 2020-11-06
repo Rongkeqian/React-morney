648a4bee6047f32eefcb1995ad7963704fe20885
RongKeQian <qianer_lover@126.com>
2020-11-06 17:23:22 +0800

    完成标签UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ import Tags from './views/Tags';
 import Money from './views/Money';
 import Statistics from './views/Statistics';
 import NoMatch from './views/NoMatch';
+import styled from 'styled-components';
 
+
+const AppWrapper = styled.div`
+color:#333;
+`
 function App() {
   return (
+    <AppWrapper>
     <Router>
           <Switch>
             <Route path="/tags">
@@ -24,6 +30,7 @@ function App() {
             </Route>
           </Switch>
     </Router>
+    </AppWrapper>
   );
 }
 
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,9 +1,81 @@
 import Layout from '../components/Layout';
 import React from 'react';
+import styled from 'styled-components';
+
+const TagSections = styled.section`
+  background:#FFF;
+  padding:0 16px;
+  >ol{
+  margin: 0 -12px;
+    >li{
+      background: #ff8a8e;
+      color: #fff;
+      display: inline-block;
+      border-radius: 18px;  
+      padding:4px 16px;
+      font-size:14px;
+      margin:6px 12px;
+    }
+  }
+  >button{
+    border:none;
+    font-family: inherit;
+    padding:2px 4px;
+    border-bottom:1px solid #333;
+    color:#666;
+    margin-top:16px;
+  }
+`
+const NotesSection =styled.section``
+
+const CategorySection = styled.section``
+
+const NumberPadSection = styled.section``
 
 function Money() {
   return(
-    <Layout><h2>记账</h2></Layout>
+    <Layout>
+       <TagSections>
+          <ol>
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+          </ol>
+         <button>新增标签</button>
+       </TagSections>
+      <NotesSection>
+        <label>
+          <span>备注</span>
+          <input type='text'/>
+        </label>
+      </NotesSection>
+      <CategorySection>
+        <ul>
+          <li>支出</li>
+          <li>收入</li>
+        </ul>
+      </CategorySection>
+      <NumberPadSection>
+        <div>100</div>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+         <button>OK</button>
+          <button>0</button>
+          <button>.</button>
+        </div>
+      </NumberPadSection>
+    </Layout>
   );
 }
 export default Money
