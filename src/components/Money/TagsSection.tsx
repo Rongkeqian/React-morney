b27ebee9838e2f8d74d961832d6c289c5c6c771e
RongKeQian <qianer_lover@126.com>
2020-11-08 11:04:09 +0800

    新增标签行为

--- src/components/Money/TagsSection.tsx
+++ src/components/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
+import React, {useState} from 'react';
 import styled from 'styled-components';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   flex-grow:1;
   background:#FFF;
   padding:0 16px;
@@ -18,6 +19,9 @@ const TagsSection = styled.section`
       padding:4px 16px;
       font-size:14px;
       margin:6px 12px;
+      &.selected{
+      background: #fc474d;
+      }
     }
   }
   >button{
@@ -28,6 +32,47 @@ const TagsSection = styled.section`
     color:#666;
     margin-top:16px;
   }
-`
+`;
 
-export {TagsSection}
+const TagsSection:React.FC = () => {
+  const [tags,setTags] = useState<string[]>(['衣','食','住','行']);
+  const [selectedTags,setSelectedTags] = useState<string[]>([])
+
+  const onAddTag =()=>{
+    const tagName =window.prompt('新标签的名称为');
+    if(tagName !== null){
+      setTags ([...tags,tagName])
+    }
+    // if(!tagName){
+    //   setTags ([...tags])
+    //  return  window.alert('标签名不能为空')
+    // }
+    // if(tagName.match(/^[ ]*$/) !== null){
+    //   setTags ([...tags])
+    //   return window.alert('请输入有效字符')
+    // }
+  }
+  const onToggleTag=(tag:string)=>{
+    const index = selectedTags.indexOf(tag)
+    if(index>=0){
+      setSelectedTags(selectedTags.filter(t=>t!==tag))
+      //如果tag已被选中，就复制所有没有被选中的tag,作为
+    }else {
+      setSelectedTags([...selectedTags,tag])
+    }
+  }
+  const getClass =(tag:string)=>{ return selectedTags.indexOf(tag) >=0 ? 'selected' :''}
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag=> <li key={tag}
+                            onClick={()=>onToggleTag(tag)}
+                           className={getClass(tag)}>
+                              {tag}
+                        </li>)}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>);
+
+};
+export {TagsSection};
