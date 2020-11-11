cc5ecfeb8f71b20c8a007d9b5cf4230ac31d8444
RongKeQian <qianer_lover@126.com>
2020-11-11 16:22:42 +0800

    tag的编辑标签名

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -22,19 +22,19 @@ const Topbar =styled.header`
     height:18px;
   }
 `
-type Params={
-  id:string
-}
+
 const InputWrapper = styled.div`
   padding:0 16px;
   background: #fff;
   margin-top: 8px;
 `
-
+type Params={
+  id:string
+}
 const Tag:React.FC=(props)=>{
-  const {findTag} = useTags();
-  let { id } =useParams<Params>()
-  const tag = findTag(parseInt(id))
+  const {findTag,updateTag} = useTags();
+  let { id:idString } =useParams<Params>()
+  const tag = findTag(parseInt(idString))
 return(
   <Layout>
     <Topbar>
@@ -43,7 +43,12 @@ return(
       <Icon name='' />
     </Topbar>
     <InputWrapper>
-      <Input label='标签名' type='text' placeholder='标签名' value ={tag.name} />
+      <Input label='标签名' type='text' placeholder='标签名'
+             value ={tag.name}
+              onChange={(e)=>{
+                updateTag(tag.id, {name:e.target.value})
+              }}
+      />
     </InputWrapper>
     <Center>
       <Space />
