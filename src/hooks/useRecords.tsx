1f945427b1ab3d849facf3942c09203c53a785cf
RongKeQian <qianer_lover@126.com>
2020-11-12 19:19:20 +0800

    å检查输入

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -20,8 +20,18 @@ export const useRecords = () => {
   }, [records]);
 
   const addRecord = (newRecord: newRecordItem) => {
+    if(newRecord.amount<=0){
+      alert('没有增加消费')
+      return false
+    }
+    if(newRecord.tagIds.length ===0){
+      alert('请选择至少一个标签')
+      return false
+    }
     const record = {...newRecord, createAt: (new Date().toISOString())};
     setRecords([...records, record]);
+
+    return true;
   };
 
 
