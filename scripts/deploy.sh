7f6a03661761b245a30bbba3ccbd8f1b6906e814
RongKeQian <qianer_lover@126.com>
2020-11-14 17:06:13 +0800

    title

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+yarn build &&
+cd build &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git remote add origin git@github.com:Rongkeqian/React-morney-website.git &&
+git branch -M main &&
+git push -u origin main &&
+cd -
+
