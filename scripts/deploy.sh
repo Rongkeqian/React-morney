df13cacf7072375f77197d440917809d7584c510
RongKeQian <qianer_lover@126.com>
2020-11-14 19:12:48 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+
 yarn build &&
 cd build &&
 git init &&
 git add . &&
 git commit -m 'deploy' &&
-git remote add origin git@github.com:Rongkeqian/React-morney-website.git &&
-git branch -M main &&
-git push -u origin main &&
+#git remote add origin git@github.com:Rongkeqian/React-morney-website.git &&
+git remote add origin git@gitee.com:mom-xiaorui/react-morney-website.git &&
+git push -u origin main  &&
 cd -
 
